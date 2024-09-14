a869397dc9fc7d200d1d8c1e833bb8d6eab82a9c
Alfredo Fernando <alfredofernando.compras@gmail.com>
2024-09-14 20:53:40 -0300

    commit inicial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Modelo Nokia1", "987654321", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("123456789", "Modelo Nokia1", "987654321", 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
